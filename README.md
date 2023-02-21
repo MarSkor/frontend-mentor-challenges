9e33ba8846948979ad21bd7ae515a18ce5692b57
MarSkor <maskor_@outlook.com>
2023-02-21 16:46:25 +0100

    js and styling fix

---
+++ README.md
@@ -0,0 +1,50 @@
+# Frontend Mentor - Intro section with dropdown navigation solution
+
+This is a solution to the [Intro section with dropdown navigation challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/intro-section-with-dropdown-navigation-ryaPetHE5). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the relevant dropdown menus on desktop and mobile when interacting with the navigation links
+- View the optimal layout for the content depending on their device's screen size
+- See hover states for all interactive elements on the page
+
+### Screenshot
+
+![](./desktop_result.png)
+
+![](./mobile_result.gif)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- Vanilla JS
+
+## Author
+
+- Frontend Mentor - [@MarSkor](https://www.frontendmentor.io/profile/MarSkor)
+- Twitter - [@marskor\_](https://twitter.com/marskor_)
--- script.js
+++ script.js
@@ -3,12 +3,14 @@ const overlay = document.querySelector(".overlay");
 const mobileMenu = document.querySelector(".mobile-menu-icon");
 const navContent = document.querySelector(".nav-content");
 const allNavItems = document.querySelectorAll(".nav-item");
-const subMenu = document.querySelectorAll(".nav-links li.sub-menu");
+const subMenu = document.querySelectorAll(".sub-menu.collapsible");
+
 
 const toggleMenu = () => {
     mobileMenu.classList.toggle("change")
     navContent.classList.toggle("showMenu")
 
+    //if nav content contains the class showMenu, activate the overlay
     if(navContent.classList.contains("showMenu")){
         overlay.classList.add('active')
     } else {
@@ -16,29 +18,34 @@ const toggleMenu = () => {
     }
 }
 
-// check if the menu icon exists, if so, do the addEventListener
+// check if the menu icon exists, if so, do toggleMenu when clicked
 if(mobileMenu){
     mobileMenu.addEventListener("click", toggleMenu);
 }
 
-// close menu when clicking link
-allNavItems.forEach((navItem) => navItem.addEventListener("click", toggleMenu))
 
 // show sub menu
-subMenu.forEach(el => el.addEventListener('click', showSubMenu))
-console.log(subMenu)
+subMenu.forEach((collapsible) => {
+    collapsible.addEventListener("click", (e) => {
+        collapsible.classList.toggle("active");
 
+        const el = e.target;
+        let img = el.querySelector("img");
 
-function showSubMenu(e){
-    const el = e.target;
+        //if the sub menu link is clicked change the arrow icon
+        if(el.classList.contains("active")){
+            el.classList.remove("active");
+            img.src = "./assets/icons/icon-arrow-down.svg"
+        } else { 
+            el.classList.add("active");
+            img.src = "./assets/icons/icon-arrow-up.svg"
+        }
 
-    let img = el.querySelector("img");
-    console.log("img", img)
-    if(el.classList.contains("active")){
-        el.classList.remove("active");
-        img.src = "./assets/icon-arrow-down.svg"
-    } else { 
-        el.classList.add("active");
-        img.src = "./assets/icon-arrow-up.svg"
-    }
-}
+        //close the current sub menu if/when another is clicked
+        subMenu.forEach((otherCollapsible) => {
+            if(otherCollapsible != collapsible){
+                otherCollapsible.classList.remove("active")
+            }
+        })
+    })
+})
